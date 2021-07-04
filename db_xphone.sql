8536cdeaa1d29977e3584e7862102a4eda98f4a5
AkhmadRifqi <81916242+AkhmadRifqi@users.noreply.github.com>
2021-07-04 19:11:23 +0700

    Add files via upload

---
+++ db_xphone.sql
@@ -0,0 +1,61 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_xphone`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_phone`
+--
+
+CREATE TABLE `tbl_phone` (
+  `id` int(4) NOT NULL,
+  `phone_name` varchar(32) NOT NULL,
+  `price` int(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tbl_phone`
+--
+
+INSERT INTO `tbl_phone` (`id`, `phone_name`, `price`) VALUES
+(1, 'oppo', 6000000),
+(2, 'realmi', 5000000),
+(3, 'samsung s20', 5000000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_phone`
+--
+ALTER TABLE `tbl_phone`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_phone`
+--
+ALTER TABLE `tbl_phone`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
